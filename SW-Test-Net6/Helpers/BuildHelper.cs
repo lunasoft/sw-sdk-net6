08e12a627401b917069b2f0e1671ff237cdd4ee9
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-10-17 23:17:43 -0500

    Add SW Test Net6

---
+++ SW-Test-Net6/Helpers/BuildHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Test.Helpers
+{
+    public class BuildHelper
+    {
+        public static string User = "pruebas_ut@sw.com.mx";
+        public static string Password = "swpass";
+    }
+}
