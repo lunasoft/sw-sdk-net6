e1790274d0046dc0ca81a3971797f47ae4e6ec25
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-10-18 18:33:01 -0500

    Stamp Services.
    - Se agrega el servicio de timbrado Stamp.
    - Se agregan los metodo internos de timbrado, mismo que son utilizados tambien por los servicios de Emision timbrado.
    - Se agregan response y response handlers.

---
+++ SW-Sdk-Net6/Services/Stamp/StampResponseHandler.cs
@@ -0,0 +1,40 @@
+﻿using SW.Handlers;
+using SW.Helpers;
+using SW.Services.Authentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.Services.Stamp
+{
+    internal class StampResponseHandlerV1 : ResponseHandler<StampResponseV1>
+    {
+        internal StampResponseV1 HandleException(Exception ex)
+        {
+            return ResponseHelper.ToStampResponseV1(ex);
+        }
+    }
+    internal class StampResponseHandlerV2 : ResponseHandler<StampResponseV2>
+    {
+        internal StampResponseV2 HandleException(Exception ex)
+        {
+            return ResponseHelper.ToStampResponseV2(ex);
+        }
+    }
+    internal class StampResponseHandlerV3 : ResponseHandler<StampResponseV3>
+    {
+        internal StampResponseV3 HandleException(Exception ex)
+        {
+            return ResponseHelper.ToStampResponseV3(ex);
+        }
+    }
+    internal class StampResponseHandlerV4 : ResponseHandler<StampResponseV4>
+    {
+        internal StampResponseV4 HandleException(Exception ex)
+        {
+            return ResponseHelper.ToStampResponseV4(ex);
+        }
+    }
+}
---
+++ SW-Sdk-Net6/Services/Stamp/StampService.cs
@@ -0,0 +1,82 @@
+﻿using SW.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace SW.Services.Stamp
+{
+    public class StampService : Services
+    {
+        string path = "/cfdi33";
+        public StampService(string url, string token, int proxyPort, string proxy) : base (url, token, proxyPort, proxy)
+        {
+        }
+        public StampService(string url, string user, string password, int proxyPort, string proxy) : base(url, user, password, proxyPort, proxy)
+        {
+        }
+        internal async Task<StampResponseV1> StampV1Async(byte[] xml, bool isB64, StampAction action)
+        {
+            StampResponseHandlerV1 handler = new StampResponseHandlerV1();
+            try
+            {
+                path = String.Format("{0}/{1}/{2}/{3}", path, action, "v1", isB64 ? "b64" : String.Empty);
+                var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
+                return await handler.PostAsync(this.Url, path, headers, proxy, xml);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+        internal async Task<StampResponseV2> StampV2Async(byte[] xml, bool isB64, StampAction action)
+        {
+            StampResponseHandlerV2 handler = new StampResponseHandlerV2();
+            try
+            {
+                path = String.Format("{0}/{1}/{2}/{3}", path, action, "v2", isB64 ? "b64" : String.Empty);
+                var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
+                return await handler.PostAsync(this.Url, path, headers, proxy, xml);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+        internal async Task<StampResponseV3> StampV3Async(byte[] xml, bool isB64, StampAction action)
+        {
+            StampResponseHandlerV3 handler = new StampResponseHandlerV3();
+            try
+            {
+                path = String.Format("{0}/{1}/{2}/{3}", path, action, "v3", isB64 ? "b64" : String.Empty);
+                var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
+                return await handler.PostAsync(this.Url, path, headers, proxy, xml);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+        internal async Task<StampResponseV4> StampV4Async(byte[] xml, bool isB64, StampAction action)
+        {
+            StampResponseHandlerV4 handler = new StampResponseHandlerV4();
+            try
+            {
+                path = String.Format("{0}/{1}/{2}/{3}", path, action, "v4", isB64 ? "b64" : String.Empty);
+                var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
+                return await handler.PostAsync(this.Url, path, headers, proxy, xml);
+            }
+            catch (Exception ex)
+            {
+                return handler.HandleException(ex);
+            }
+        }
+    }
+}
