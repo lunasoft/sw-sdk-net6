c2dc8cd5ddce69a018106c9ed0805e04e74e059e
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-10-26 22:15:07 -0500

    Actualizar servicios de timbrado CFDI
    - Se actualizan para soportar parametros extras provenientes de las clase base de timbrado V4.

--- SW-Sdk-Net6/Services/IssueJson/IssueJsonService.cs
+++ SW-Sdk-Net6/Services/IssueJson/IssueJsonService.cs
@@ -5,7 +5,7 @@ namespace SW.Services.IssueJson
 {
     public class IssueJsonService : Services
     {
-        private readonly string _path = "/v3/cfdi33/issue/json";
+        private readonly string _path = "cfdi33/issue/json";
         private readonly string _contentType = "application/jsontoxml";
         public IssueJsonService(string url, string user, string password, int proxyPort, string proxy) : base (url, user, password, proxyPort, proxy)
         {
@@ -13,56 +13,88 @@ namespace SW.Services.IssueJson
         public IssueJsonService(string url, string token, int proxyPort, string proxy) : base (url, token, proxyPort, proxy)
         {
         }
-        internal async Task<StampResponseV1> IssueJsonV1Async(string json)
+        internal async Task<StampResponseV1> IssueJsonV1Async(string json, StampVersion version, string customId = null, string[] email = null, bool pdf = false)
         {
-            StampResponseHandlerV1 handler = new ();
+            StampResponseHandlerV1 handler = new();
             try
             {
                 var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                if (version.Equals(StampVersion.V4))
+                {
+                    if (!ValidationHelper.ValidateCustomHeaders(customId, email, out string message))
+                    {
+                        throw new Exception(message);
+                    }
+                    headers = RequestHelper.SetupHeaders(headers, customId, email);
+                }
                 var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
-                return await handler.PostAsync(this.Url, String.Format("{0}/{1}", _path, "v1"), headers, proxy, json, _contentType);
+                return await handler.PostAsync(this.Url, String.Format("{0}/{1}/{2}", version, _path, StampResponseVersion.V1), headers, proxy, json, _contentType);
             }
             catch(Exception ex)
             {
                 return handler.HandleException(ex);
             }
         }
-        internal async Task<StampResponseV2> IssueJsonV2Async(string json)
+        internal async Task<StampResponseV2> IssueJsonV2Async(string json, StampVersion version, string customId = null, string[] email = null, bool pdf = false)
         {
-            StampResponseHandlerV2 handler = new ();
+            StampResponseHandlerV2 handler = new();
             try
             {
                 var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                if (version.Equals(StampVersion.V4))
+                {
+                    if (!ValidationHelper.ValidateCustomHeaders(customId, email, out string message))
+                    {
+                        throw new Exception(message);
+                    }
+                    headers = RequestHelper.SetupHeaders(headers, customId, email);
+                }
                 var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
-                return await handler.PostAsync(this.Url, String.Format("{0}/{1}", _path, "v2"), headers, proxy, json, _contentType);
+                return await handler.PostAsync(this.Url, String.Format("{0}/{1}/{2}", version, _path, StampResponseVersion.V2), headers, proxy, json, _contentType);
             }
             catch (Exception ex)
             {
                 return handler.HandleException(ex);
             }
         }
-        internal async Task<StampResponseV3> IssueJsonV3Async(string json)
+        internal async Task<StampResponseV3> IssueJsonV3Async(string json, StampVersion version, string customId = null, string[] email = null, bool pdf = false)
         {
-            StampResponseHandlerV3 handler = new ();
+            StampResponseHandlerV3 handler = new();
             try
             {
                 var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                if (version.Equals(StampVersion.V4))
+                {
+                    if (!ValidationHelper.ValidateCustomHeaders(customId, email, out string message))
+                    {
+                        throw new Exception(message);
+                    }
+                    headers = RequestHelper.SetupHeaders(headers, customId, email);
+                }
                 var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
-                return await handler.PostAsync(this.Url, String.Format("{0}/{1}", _path, "v3"), headers, proxy, json, _contentType);
+                return await handler.PostAsync(this.Url, String.Format("{0}/{1}/{2}", version, _path, StampResponseVersion.V3), headers, proxy, json, _contentType);
             }
             catch (Exception ex)
             {
                 return handler.HandleException(ex);
             }
         }
-        internal async Task<StampResponseV4> IssueJsonV4Async(string json)
+        internal async Task<StampResponseV4> IssueJsonV4Async(string json, StampVersion version, string customId = null, string[] email = null, bool pdf = false)
         {
-            StampResponseHandlerV4 handler = new ();
+            StampResponseHandlerV4 handler = new();
             try
             {
                 var headers = await RequestHelper.SetupAuthHeaderAsync(this);
+                if (version.Equals(StampVersion.V4))
+                {
+                    if (!ValidationHelper.ValidateCustomHeaders(customId, email, out string message))
+                    {
+                        throw new Exception(message);
+                    }
+                    headers = RequestHelper.SetupHeaders(headers, customId, email);
+                }
                 var proxy = RequestHelper.ProxySettings(this.Proxy, this.ProxyPort);
-                return await handler.PostAsync(this.Url, String.Format("{0}/{1}", _path, "v4"), headers, proxy, json, _contentType);
+                return await handler.PostAsync(this.Url, String.Format("{0}/{1}/{2}", version, _path, StampResponseVersion.V4), headers, proxy, json, _contentType);
             }
             catch (Exception ex)
             {
