f4342da0f41cb2af58f80328efdcf6b2571e8e55
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-10-28 18:48:20 -0500

    UT B64

--- SW-Test-Net6/Services/Issue/IssueV4StorageTest.cs
+++ SW-Test-Net6/Services/Issue/IssueV4StorageTest.cs
@@ -5,7 +5,7 @@ using SW.Test.Helpers;
 namespace SW.Test.Services.IssueTest
 {
     [TestClass]
-    public class IssueJsonV4StorageTest
+    public class IssueV4StorageTest
     {
         [TestMethod]
         public async Task IssueV4Storage_V2_CFDI3307_Success()
@@ -62,6 +62,67 @@ namespace SW.Test.Services.IssueTest
             var xml = await BuildHelper.GetCfdi("cfdi40.xml");
             var response = await stamp.StampV2Async(xml, Guid.NewGuid().ToString());
             response = await stamp.StampV2Async(xml, Guid.NewGuid().ToString());
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("307. El comprobante contiene un timbre previo."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Cfdi));
+        }
+        [TestMethod]
+        public async Task IssueV4Storage_V2_CFDI3307_B64_Success()
+        {
+            var stamp = new IssueV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.Token);
+            var xml = await BuildHelper.GetCfdi("cfdi40.xml", true);
+            var customId = Guid.NewGuid().ToString();
+            var response = await stamp.StampV2Async(xml, customId, false, true);
+            xml = await BuildHelper.GetCfdi("cfdi40.xml", true);
+            Thread.Sleep(5000);
+            response = await stamp.StampV2Async(xml, customId, false, true);
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Cfdi));
+        }
+        [TestMethod]
+        public async Task IssueV4Storage_V2_CFDI3307_B64_Auth_Success()
+        {
+            var stamp = new IssueV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.User, BuildHelper.Password);
+            var xml = await BuildHelper.GetCfdi("cfdi40.xml", true);
+            var customId = Guid.NewGuid().ToString();
+            var response = await stamp.StampV2Async(xml, customId, false, true);
+            xml = await BuildHelper.GetCfdi("cfdi40.xml", true);
+            Thread.Sleep(5000);
+            response = await stamp.StampV2Async(xml, customId, false, true);
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Cfdi));
+        }
+        [TestMethod]
+        public async Task IssueV4Storage_V2_CFDI3307_B64_Error()
+        {
+            var stamp = new IssueV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.Token);
+            var xmlA = await BuildHelper.GetCfdi("cfdi40.xml", true);
+            var xmlB = await BuildHelper.GetCfdi("cfdi40.xml", true);
+            var customId = Guid.NewGuid().ToString();
+            var response = await stamp.StampV2Async(xmlA, customId, false, true);
+            response = await stamp.StampV2Async(xmlB, customId, false, true);
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado."));
+            Assert.IsTrue(response.MessageDetail.Equals("No se pudo obtener la URL de descarga."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(String.IsNullOrEmpty(response.Data.Cfdi));
+        }
+        [TestMethod]
+        public async Task IssueV4Storage_V2_307_B64_Error()
+        {
+            var stamp = new IssueV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.Token);
+            var xml = await BuildHelper.GetCfdi("cfdi40.xml", true);
+            var response = await stamp.StampV2Async(xml, Guid.NewGuid().ToString(), false, true);
+            response = await stamp.StampV2Async(xml, Guid.NewGuid().ToString(), false, true);
             Assert.IsTrue(response.Status.Equals("error"));
             Assert.IsTrue(response.Message.Equals("307. El comprobante contiene un timbre previo."));
             Assert.IsNotNull(response.Data);
--- SW-Test-Net6/Services/Stamp/StampV4StorageTest.cs
+++ SW-Test-Net6/Services/Stamp/StampV4StorageTest.cs
@@ -72,5 +72,66 @@ namespace SW.Test.Services.StampTest
             Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
             Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Cfdi));
         }
+        [TestMethod]
+        public async Task StampV4Storage_V2_CFDI3307_B64_Success()
+        {
+            var stamp = new StampV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.Token);
+            var xml = await BuildHelper.GetCfdi("cfdi40.xml", true, true);
+            var customId = Guid.NewGuid().ToString();
+            var response = await stamp.StampV2Async(xml, customId, false, true);
+            xml = await BuildHelper.GetCfdi("cfdi40.xml", true, true);
+            Thread.Sleep(1500);
+            response = await stamp.StampV2Async(xml, customId, false, true);
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Cfdi));
+        }
+        [TestMethod]
+        public async Task StampV4Storage_V2_CFDI3307_B64_Auth_Success()
+        {
+            var stamp = new StampV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.User, BuildHelper.Password);
+            var xml = await BuildHelper.GetCfdi("cfdi40.xml", true, true);
+            var customId = Guid.NewGuid().ToString();
+            var response = await stamp.StampV2Async(xml, customId, false, true);
+            xml = await BuildHelper.GetCfdi("cfdi40.xml", true, true);
+            Thread.Sleep(1500);
+            response = await stamp.StampV2Async(xml, customId, false, true);
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Cfdi));
+        }
+        [TestMethod]
+        public async Task StampV4Storage_V2_CFDI3307_B64_Error()
+        {
+            var stamp = new StampV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.Token);
+            var xmlA = await BuildHelper.GetCfdi("cfdi40.xml", true, true);
+            var xmlB = await BuildHelper.GetCfdi("cfdi40.xml", true, true);
+            var customId = Guid.NewGuid().ToString();
+            var response = await stamp.StampV2Async(xmlA, customId, false, true);
+            response = await stamp.StampV2Async(xmlB, customId, false, true);
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("CFDI3307 - Timbre duplicado. El customId proporcionado está duplicado."));
+            Assert.IsTrue(response.MessageDetail.Equals("No se pudo obtener la URL de descarga."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(String.IsNullOrEmpty(response.Data.Cfdi));
+        }
+        [TestMethod]
+        public async Task StampV4Storage_V2_307_B64_Error()
+        {
+            var stamp = new StampV4Storage(BuildHelper.UrlApi, BuildHelper.UrlService, BuildHelper.Token);
+            var xml = await BuildHelper.GetCfdi("cfdi40.xml", true, true);
+            var response = await stamp.StampV2Async(xml, Guid.NewGuid().ToString(), false, true);
+            response = await stamp.StampV2Async(xml, Guid.NewGuid().ToString(), false, true);
+            Assert.IsTrue(response.Status.Equals("error"));
+            Assert.IsTrue(response.Message.Equals("307. El comprobante contiene un timbre previo."));
+            Assert.IsNotNull(response.Data);
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Tfd));
+            Assert.IsTrue(!String.IsNullOrEmpty(response.Data.Cfdi));
+        }
     }
 }
