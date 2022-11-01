f12091372aef6e7b3b28071c593081eb020ef71a
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2022-11-01 12:10:59 -0600

    Agregar String extension para b64 convertions.

---
+++ SW-Sdk-Net6/Helpers/StringExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+namespace SW.Helpers
+{
+    internal static class StringExtension
+    {
+        internal static string ConvertToB64(this string str)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(str));
+        }
+        internal static string ConvertFromB64(this string b64)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(b64));
+        }
+    }
+}
