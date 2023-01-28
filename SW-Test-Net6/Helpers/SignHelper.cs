9fc0efeca923dc52554f7f5574bc611b25ee4f85
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-01-27 21:55:19 -0600

    Update helpers to sign the XML

--- SW-Test-Net6/Helpers/SignHelper.cs
+++ SW-Test-Net6/Helpers/SignHelper.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Security.Cryptography.X509Certificates;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Xsl;
 using System.Xml;
 
@@ -12,93 +8,77 @@ namespace SW.Test.Helpers
 {
     public class SignHelper
     {
-        static Random randomNumber = new Random(1);
-
         internal static string SignCfdi(byte[] pfx, string password, string xml)
-        {
-            xml = RemoverCaracteresInvalidosXml(xml);
-            X509Certificate2 x509Certificate = new X509Certificate2(pfx, password
-                 , X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
-
-            //Set values Certificate
-            XmlDocument doc = new XmlDocument();
-            doc.LoadXml(xml);
-            doc.DocumentElement.SetAttribute("NoCertificado", CertificateNumber(x509Certificate));
-            doc.DocumentElement.SetAttribute("Certificado", Convert.ToBase64String(x509Certificate.GetRawCertData()));
-            using (MemoryStream ms = new MemoryStream())
-            {
-                doc.Save(ms);
-                ms.Seek(0, SeekOrigin.Begin);
-                xml = RemoverCaracteresInvalidosXml(Encoding.UTF8.GetString(ms.ToArray()));
-            }
-            //Get original string
-            string originalString = CadenaOriginalCFDI40(xml);
-            //Sign Document
-            var signResult = GetSignature(password, pfx, originalString, "SHA256");
-            //Set Values Signature
-            doc = new XmlDocument();
-            doc.LoadXml(xml);
-            doc.DocumentElement.SetAttribute("Sello", signResult);
-            using (MemoryStream ms = new MemoryStream())
-            {
-                doc.Save(ms);
-                ms.Seek(0, SeekOrigin.Begin);
-                xml = RemoverCaracteresInvalidosXml(Encoding.UTF8.GetString(ms.ToArray()));
-            }
-            return xml;
-        }
-
-        internal static string CadenaOriginalCFDI40(string strXml)
         {
             try
             {
-                var xslt = new XslCompiledTransform();
-                XsltSettings settings = new XsltSettings(true, true);
-                XmlUrlResolver resolver = new XmlUrlResolver();
-                xslt.Load("Resources/Xslt/cadenaoriginal_4_0.xslt", settings, resolver);
-                string resultado = null;
-                StringWriter writer = new StringWriter();
-                XmlReader xml = XmlReader.Create(new StringReader(strXml));
-                xslt.Transform(xml, null, writer);
-                resultado = writer.ToString().Trim();
-                writer.Close();
-
-                return resultado;
+                X509Certificate2 x509Certificate = new(pfx, password
+                 , X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
+                XmlDocument doc = new();
+                doc.LoadXml(xml);
+                doc.DocumentElement.SetAttribute("NoCertificado", CertificateNumber(x509Certificate));
+                doc.DocumentElement.SetAttribute("Certificado", Convert.ToBase64String(x509Certificate.GetRawCertData()));
+                xml = SaveXmlDocument(doc);
+                string originalString = GetCadenaOriginal(xml);
+                var signResult = GetSello(pfx, password, originalString);
+                doc = new();
+                doc.LoadXml(xml);
+                doc.DocumentElement.SetAttribute("Sello", signResult);
+                return SaveXmlDocument(doc);
             }
             catch (Exception ex)
             {
-
-                throw new Exception("El XML proporcionado no es válido.", ex);
+                return ex.Message;
             }
         }
-
-        private static string GetSignature(string password, byte[] pfx, string strToSign, string algorithm = "SHA1")
+        private static string GetCadenaOriginal(string xml)
         {
-            var signData = string.Empty;
-            RSACryptoServiceProvider rsa = default(RSACryptoServiceProvider);
-            byte[] signatureBytes = default(byte[]);
-            X509Certificate2 certX509 = new X509Certificate2(pfx, password
-                 , X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable);
-
-            rsa = new RSACryptoServiceProvider();
-            RSAKeyHelper.FromXmlString(rsa, RSAKeyHelper.ToXmlString(certX509.GetRSAPrivateKey(), true));
-            byte[] data = Encoding.UTF8.GetBytes(strToSign);
-
-            signatureBytes = rsa.SignData(data, CryptoConfig.MapNameToOID(algorithm));
-            return Convert.ToBase64String(signatureBytes);
+            XsltSettings settings = new(true, true);
+            XmlUrlResolver resolver = new();
+            XslCompiledTransform xslt = new();
+            xslt.Load("Resources/Xslt/cadenaoriginal_4_0.xslt", settings, resolver);
+            string transformedXml = String.Empty;
+            XmlReader xmlReader = XmlReader.Create(new StringReader(xml));
+            using (StringWriter stringWriter = new())
+            {
+                xslt.Transform(xmlReader, null, stringWriter);
+                transformedXml = stringWriter.ToString();
+                xmlReader.Close();
+                stringWriter.Close();
+            }
+            return transformedXml;
         }
-
+        private static string GetSello(byte[] pfx, string password, string cadenaOriginal)
+        {
+            X509Certificate2 certificate = new(pfx, password);
+            var rsaPrivKey = certificate.GetRSAPrivateKey();
+            CngKey privateKey = rsaPrivKey is RSACng rsaCng ? rsaCng.Key : CngKey.Open("");
+            RSACng rsa = new(privateKey);
+            byte[] signature = rsa.SignData(Encoding.UTF8.GetBytes(cadenaOriginal), HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+            return Convert.ToBase64String(signature);
+        }
         private static string CertificateNumber(X509Certificate2 cert)
         {
             string hexadecimalString = cert.SerialNumber;
-            StringBuilder sb = new StringBuilder();
+            StringBuilder sb = new();
             for (int i = 0; i <= hexadecimalString.Length - 2; i += 2)
             {
                 sb.Append(Convert.ToString(Convert.ToChar(Int32.Parse(hexadecimalString.Substring(i, 2), System.Globalization.NumberStyles.HexNumber))));
             }
             return sb.ToString();
         }
-
+        private static string SaveXmlDocument(XmlDocument xml)
+        {
+            string strXml = String.Empty;
+            using (MemoryStream ms = new())
+            {
+                xml.Save(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                strXml = RemoverCaracteresInvalidosXml(Encoding.UTF8.GetString(ms.ToArray()));
+                ms.Close();
+            }
+            return strXml;
+        }
         internal static string RemoverCaracteresInvalidosXml(string xmlInvoice)
         {
             xmlInvoice = xmlInvoice.Replace("\r\n", "");
@@ -108,6 +88,22 @@ namespace SW.Test.Helpers
             xmlInvoice = xmlInvoice.Replace(@"
 ", "");
             return xmlInvoice;
+        }
+        internal static byte[] CreatePfx(byte[] certificate, byte[] privateKey, string password)
+        {
+            try
+            {
+                var rsa = RSA.Create();
+                rsa.ImportEncryptedPkcs8PrivateKey(password, privateKey, out _);
+                var cert = new X509Certificate2(certificate, password,
+                    X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.Exportable | X509KeyStorageFlags.PersistKeySet);
+                var x509Certificate = cert.CopyWithPrivateKey(rsa);
+                return x509Certificate.Export(X509ContentType.Pfx, password);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
