945fd91ba19da0ae263ea9b64227402e13416335
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-01-31 18:33:19 -0600

    Add Request Entity

---
+++ SW-Sdk-Net6/Entities/Request.cs
@@ -0,0 +1,9 @@
+﻿namespace SW.Entities
+{
+    internal class Request
+    {
+        public string? Path { get; set; }
+        public Dictionary<string, string> Headers { get; set; }
+        public HttpClientHandler? Proxy { get; set; }
+    }
+}
